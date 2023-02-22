3587ae32a7d40e5fbbbff2f6381c1e4246d0979d
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-02-22 17:26:30 -0500

    completed lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,13 @@
 # your code goes here!
+def countdown(num):
+    while num >= 0: 
+        if num >= 1 :
+            print(f"{num} Second(S)!")
+        elif num >= 0:
+            print("HAPPY NEW YEAR!")
+        num -= 1
+
+
+
+
+countdown(10)
